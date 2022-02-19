bdbed86cc78cf1b4b088d6339e0cc04cae726ce7
andipermana444 <raditya.putra.andi@gmail.com>
2022-02-19 16:36:37 +0700

    belajar variabel

---
+++ variabel.py
@@ -0,0 +1,26 @@
+# variabel adalah tempat menyimpan data
+
+# menaru / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)  # alt+panah=pindah
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 # ini boleh (nomor gaboleh didepan)
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)  # nilai sebelum diubah
+a = 7
+print("Nilai a = ", a)  # nilai sesudah diubah
+
+# assignmant indirect
+b = a
+print("Nilai b = ", b)  # block ctrl+d = clone
+
