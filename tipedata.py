34a4e879a43fe783d4a555f819ed356444519bbc
andipermana444 <raditya.putra.andi@gmail.com>
2022-02-19 16:38:35 +0700

    belajar tipe data

---
+++ tipedata.py
@@ -0,0 +1,8 @@
+# a = 10, a adalah variabel dengan nilai 10
+
+# tipe data: Angka satuan (integer)
+data_integer = 1   # berapapun nilainya akan keluar integer
+print("data : ", data_integer)
+print("bertipe : ", type(data_integer))
+
+# tipe data: angka dengan point (float)
