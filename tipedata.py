697655c49d933b7ab6b8c8a85a7ebb801e8b24ea
andipermana444 <raditya.putra.andi@gmail.com>
2022-02-19 17:30:32 +0700

    penyempurnaan dasar

--- tipedata.py
+++ tipedata.py
@@ -1,8 +1,29 @@
 # a = 10, a adalah variabel dengan nilai 10
 
-# tipe data: Angka satuan (integer)
-data_integer = 1   # berapapun nilainya akan keluar integer
+# tipe data: Angka satuan yang tidak ada . (integer)
+
+data_integer = 11   # berapapun nilainya akan keluar integer
 print("data : ", data_integer)
 print("bertipe : ", type(data_integer))
 
 # tipe data: angka dengan point (float)
+data_float = 1.5
+print("data : ", data_float)
+print("bertipe : ", type(data_float))
+
+# tipe data: kumpulan karakter (string) string =""
+data_string = "friskies"
+print("data : ", data_string)
+print("bertipe : ", type(data_string))
+
+# tipe data: biner true/false (boolean) hanya bisa T/F
+data_bool = True
+print("data : ", data_bool)
+print("bertipe : ", type(data_bool))
+
+## tipe data khusus
+
+# bilangan kompleks  (menggunakan koma)
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("bertiper : ", type(data_complex))
