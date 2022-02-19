1df8165166120ee2973e15245f0e3bb94507b043
andipermana444 <raditya.putra.andi@gmail.com>
2022-02-19 17:33:57 +0700

    penambahan data

--- tipedata.py
+++ tipedata.py
@@ -27,3 +27,11 @@ print("bertipe : ", type(data_bool))
 data_complex = complex(5,6)
 print("data : ", data_complex)
 print("bertiper : ", type(data_complex))
+
+# tipe data dari bahasa C
+
+from ctypes import c_double, c_char
+
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("bertipe : ", type(data_c_double))
